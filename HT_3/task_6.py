c346139a3b19ab87b2e1d6605510cbbe25c43fdf
Igor <46813877+useronelex@users.noreply.github.com>
2022-10-14 07:04:24 +0000

    Update task_6.py

--- HT_3/task_6.py
+++ HT_3/task_6.py
@@ -5,7 +5,4 @@ dict = {'foo': '1',
         'baz': '3'
         }
 
-min_key = min(dict, key = dict.get)
-max_key = max(dict, key = dict.get)
-
-print(f'Min value in dictionary: {dict.get(min_key)}\nMax value in dictionary: {dict.get(max_key)}')
+print(f'Min value in dictionary: {min(dict.values())}\nMax value in dictionary: {max(dict.values())}')
