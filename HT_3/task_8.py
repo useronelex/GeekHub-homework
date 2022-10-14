782e5b6138fe461f42427577eaefce7a200aa9d5
Igor <46813877+useronelex@users.noreply.github.com>
2022-10-14 08:28:59 +0000

    Update task_8.py

--- HT_3/task_8.py
+++ HT_3/task_8.py
@@ -1,11 +1,10 @@
 ''' Створити цикл від 0 до ... (вводиться користувачем). В циклі створити умову, яка буде виводити поточне значення, якщо остача від ділення на 17 дорівнює 0. '''
 
 try:
-   number = int(input('Введіть число: '))
-   counter = 0
-   while counter < number:
-      counter += 1
-      if counter % 17 == 0:
-         print(counter)
+    number = int(input('Введіть число: '))
+    for i in range(number):
+        if i % 17 == 0:
+            print(i)
+
 except ValueError as err:
-   print(f'Enter corect value. {err} is not integer number.') 
+    print(f'Enter corect value. {err} is not integer number.') 
