9e50794869e693eba53c9980e4cee51f9a1c7bc1
Igor <46813877+useronelex@users.noreply.github.com>
2022-10-14 08:46:10 +0000

    Update task_7.py

--- HT_3/task_7.py
+++ HT_3/task_7.py
@@ -1,13 +1,7 @@
 ''' Write a script which accepts a <number>(int) from user and generates dictionary in range <number> where key is <number> and value is <number>*<number>
     e.g. 3 --> {0: 0, 1: 1, 2: 4, 3: 9} '''
 
-number = int(input('Enter integer number: '))
-list_numers = []
-i = 0
+n = int(input('Enter integer number: '))
 
-while i < number:
-   list_numers.append(i)
-   i += 1
-dictionary = {el: el*el for el in list_numers}
-
+dictionary = {i: i*i for i in range(n)}
 print(dictionary)
