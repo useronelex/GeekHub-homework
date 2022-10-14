cff7bc75e1a3c2b019318ca4fd5fee566579d583
Igor <46813877+useronelex@users.noreply.github.com>
2022-10-14 08:56:13 +0000

    Update task_9.py

--- HT_3/task_9.py
+++ HT_3/task_9.py
@@ -1,15 +1,14 @@
 '''  Користувачем вводиться початковий і кінцевий рік. Створити цикл, який виведе всі високосні роки в цьому проміжку (границі включно). P.S. Рік є високосним, якщо він кратний 4, але не кратний 100, а також якщо він кратний 400. '''
 
 try:
-   first_year = int(input('Enter first year: '))
-   last_year = int(input('Enter last year: '))
-   while first_year <= last_year:
-      if first_year %4 == 0 and first_year % 100 != 0:
-         print(first_year)
-      elif first_year % 400 == 0:
-         print(first_year)
-      first_year += 1
-   else:
-      print('First entered year has more value then value last entered  year')
+    first_year = int(input('Enter first year: '))
+    last_year = int(input('Enter last year: '))
+
+    for i in range(first_year, last_year):
+        if i %4 == 0 and i % 100 != 0:
+            print(i)
+        elif i % 400 == 0:
+            print(i)
+
 except ValueError:
-   print(f'Enter the year as a number.\nFor example: 2000')
+    print(f'Enter the year as a number.\nFor example: 2000')
