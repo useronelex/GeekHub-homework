36ff5e3a4fcf238143f0657816d2595d0d361db2
Igor <46813877+useronelex@users.noreply.github.com>
2022-10-17 09:02:27 +0000

    Update task_1.py

--- HT_4/task_1.py
+++ HT_4/task_1.py
@@ -3,18 +3,18 @@
 def season(month):
     try:
         if month <= 0 or month > 12:
-            print ('The month value must be in the range 0 to 12')
+            print('The month value must be in the range 0 to 12')
         else:
             if month <= 2 or month == 12:
-                print ('winter')
+                print('winter')
             elif 2 < month <= 5:
-                print ('spring')
+                print('spring')
             elif 5 < month <= 8:
-                print ('summer')
+                print('summer')
             else:
                 print('autumn')
     except TypeError:
-        print ('Wrong data type entered.\nPleace, enter number from 0 to 12.')                 
+        print('Wrong data type entered.\nPleace, enter number from 0 to 12.')                 
          
                 
 season(12)    
