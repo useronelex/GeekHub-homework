05910b2d5c46e3df6c87f532fc1cab11dd834bab
Igor <46813877+useronelex@users.noreply.github.com>
2022-10-17 07:55:57 +0000

    Update task_1.py

--- HT_3/task_1.py
+++ HT_3/task_1.py
@@ -1,16 +1,14 @@
 ''' Write a script that will run through a list of tuples and replace the last value for each tuple. The list of tuples can be hardcoded. The "replacement" value is entered by user. The number of elements in the tuples must be different. '''
 
-value = input('Enter somethin: ')
-tuple_list = [(1, 2, 3), (4, 5, 6, 6.5, 6.75), (7, 8, 9,10), ()]
+value = input('Enter some values: ')
+tuple_list = [(1, 2, 3), (), (4, 5, 6, 6.5, 6.75), (7, 8, 9,10), ()]
+
 new_tuple_list = []
 
-if all(tuple_list):
-    for elem_list in tuple_list:
-        ls_tuple_elements = list(elem_list)
-        ls_tuple_elements.pop()
-        ls_tuple_elements.append(value)
-        elem_list = tuple(ls_tuple_elements)
-        new_tuple_list.append(elem_list)
-    print(new_tuple_list)    
-else:
-    print('You have empty tuple')
+for element_list in tuple_list:
+    if element_list:
+        new_tuple_list += [element_list[:-1] + (value,)]  
+    else: 
+        new_tuple_list += (element_list, )
+
+print(new_tuple_list)
