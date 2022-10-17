ef9f7016f4d8d4cc1510add4da962c27ceb3d1a2
Igor <46813877+useronelex@users.noreply.github.com>
2022-10-17 08:43:03 +0000

    Update task_1.py

--- HT_3/task_1.py
+++ HT_3/task_1.py
@@ -6,8 +6,8 @@ new_tuple_list = []
 
 for element_list in tuple_list:
     if element_list:
-        new_tuple_list += [element_list[:-1] + (value,)]  
+        new_tuple_list.append([element_list[:-1] + (value,)])
     else: 
-        new_tuple_list += (element_list, )
+        new_tuple_list.append((element_list, ))
 
 print(new_tuple_list)
