cbe47e93f4e29dda6de8b1c5b837330efc6dd3d4
Igor <46813877+useronelex@users.noreply.github.com>
2022-10-20 15:32:39 +0300

    Update task_2.py

--- HT_5/task_2.py
+++ HT_5/task_2.py
@@ -1,15 +1,14 @@
 ''' Написати функцію <bank> , яка працює за наступною логікою: користувач робить вклад у розмірі <a> одиниць строком на <years> років під <percents> відсотків (кожен рік сума вкладу збільшується на цей відсоток, ці гроші додаються до суми вкладу і в наступному році на них також нараховуються відсотки). Параметр <percents> є необов'язковим і має значення по замовчуванню <10> (10%). Функція повинна принтануть суму, яка буде на рахунку, а також її повернути (але округлену до копійок). '''
 
-
 def bank(a, years, percent= 10):
-    percent_a = 0
-    percent_a += a * (percent/100)
+    sum_percent_a = 0
+    p = percent / 100
     for i in range(years):
-        a += percent_a    
-          
+        sum_percent_a = a * p
+        a += sum_percent_a  
     return round(a)
 
 try:    
-    print(bank(350.8, 3, 4.5))
+    print(bank(100, 2, 10))
 except (ValueError, TypeError):
     print('Invalid value')
